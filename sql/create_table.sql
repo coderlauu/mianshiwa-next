6026655dbfc0715b25c0a33ae496744d63376cf6
coderlauu <coderlauu@example.com>
2025-11-09 14:38:44 +0800

    初始化项目模板

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://yupi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists mianshiwa;
 
 -- 切换库
-use my_db;
+use mianshiwa;
 
 -- 用户表
 create table if not exists user
@@ -20,48 +20,78 @@ create table if not exists user
     userAvatar   varchar(1024)                          null comment '用户头像',
     userProfile  varchar(512)                           null comment '用户简介',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    editTime     datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
     index idx_unionId (unionId)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 帖子表
-create table if not exists post
+-- 题库表
+create table if not exists question_bank
+(
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(256)                       null comment '标题',
+    description text                               null comment '描述',
+    picture     varchar(2048)                      null comment '图片',
+    userId      bigint                             not null comment '创建用户 id',
+    editTime    datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title)
+) comment '题库' collate = utf8mb4_unicode_ci;
+
+-- 题目表
+create table if not exists question
 (
     id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
+    title      varchar(256)                       null comment '标题',
     content    text                               null comment '内容',
     tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
+    answer     text                               null comment '推荐答案',
     userId     bigint                             not null comment '创建用户 id',
+    editTime   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
     createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete   tinyint  default 0                 not null comment '是否删除',
+    index idx_title (title),
     index idx_userId (userId)
-) comment '帖子' collate = utf8mb4_unicode_ci;
+) comment '题目' collate = utf8mb4_unicode_ci;
 
--- 帖子点赞表（硬删除）
-create table if not exists post_thumb
+-- 题库题目表（硬删除）
+create table if not exists question_bank_question
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
-    index idx_userId (userId)
-) comment '帖子点赞';
+    id             bigint auto_increment comment 'id' primary key,
+    questionBankId bigint                             not null comment '题库 id',
+    questionId     bigint                             not null comment '题目 id',
+    userId         bigint                             not null comment '创建用户 id',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    UNIQUE (questionBankId, questionId)
+) comment '题库题目' collate = utf8mb4_unicode_ci;
 
--- 帖子收藏表（硬删除）
-create table if not exists post_favour
+-- 功能扩展：用户个人主页资料修改
+ALTER TABLE user
+    ADD phoneNumber        VARCHAR(20) COMMENT '手机号',
+    ADD email              VARCHAR(256) COMMENT '邮箱',
+    ADD grade              VARCHAR(50) COMMENT '年级',
+    ADD workExperience     VARCHAR(512) COMMENT '工作经验',
+    ADD expertiseDirection VARCHAR(512) COMMENT '擅长方向';
+
+-- 功能扩展：AI 模拟面试功能
+-- 模拟面试表
+create table if not exists mock_interview
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
+    id             bigint auto_increment comment 'id' primary key,
+    workExperience varchar(256)                       not null comment '工作年限',
+    jobPosition    varchar(256)                       not null comment '工作岗位',
+    difficulty     varchar(50)                        not null comment '面试难度',
+    messages       mediumtext                         null comment '消息列表（JSON 对象数组字段，同时包括了总结）',
+    status         int      default 0                 not null comment '状态（0-待开始、1-进行中、2-已结束）',
+    userId         bigint                             not null comment '创建人（用户 id）',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete       tinyint  default 0                 not null comment '是否删除（逻辑删除）',
     index idx_userId (userId)
-) comment '帖子收藏';
+) comment '模拟面试' collate = utf8mb4_unicode_ci;
